a7d293600a1cdae771bd944e535110a7e048d37c
Victor Krause <victor5887@gmail.com>
2020-06-29 05:46:31 -0300

    Ajustes e testes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,3 +11,15 @@ INSERT INTO billionaires (first_name, last_name, career) VALUES
   ('Aliko', 'Dangote', 'Billionaire Industrialist'),
   ('Bill', 'Gates', 'Billionaire Tech Entrepreneur'),
   ('Folrunsho', 'Alakija', 'Billionaire Oil Magnate');
+
+
+CREATE TABLE IF NOT EXISTS ARVORE_DIRETORIOS  (
+  ID INT AUTO_INCREMENT  PRIMARY KEY,
+  NAME VARCHAR(250) NOT NULL,
+  PARENT_ID INT NOT NULL,
+  CONTENT BLOB NULL
+);
+
+INSERT INTO ARVORE_DIRETORIOS  SELECT * FROM (
+SELECT 0, 'ROOT', -1, NULL
+) X WHERE NOT EXISTS(SELECT * FROM ARVORE_DIRETORIOS);
